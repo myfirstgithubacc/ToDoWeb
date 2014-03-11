efc14690f15698fd4160d8a6e51edc44c8da915a
takakiyo (at home) <t.takakiy@gmail.com>
2014-03-12 08:51:45 +0900

    Start to create Sample Application
    Sample Application for Liberty Profile Article on developerWorks.

---
+++ src/com/ibm/jp/sample/todo/todo/ToDoEntry.java
@@ -0,0 +1,43 @@
+package com.ibm.jp.sample.todo.todo;
+
+import java.io.Serializable;
+
+public class ToDoEntry implements Serializable {
+
+	private String id;
+	private String title;
+	private String detail;
+	
+	public ToDoEntry(String id, String title, String detail) {
+		if (id == null) {
+			throw new NullPointerException("ToDoEntry: id is null");
+		}
+		this.id = id;
+		this.title = title;
+		this.detail = detail;
+	}
+	public String getId() {
+		return id;
+	}
+	public String getTitle() {
+		return title;
+	}
+	public String getDetail() {
+		return detail;
+	}
+
+	@Override
+	public int hashCode() {
+		return id.hashCode();
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (obj instanceof ToDoEntry) {
+			ToDoEntry other = (ToDoEntry) obj;
+			return this.id.equals(other.id);
+		} else {
+			return false;
+		}
+	}
+}
